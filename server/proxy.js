a5bc2b500a7ab32ac292c71a9edd0206d9a5fe62
peng wu <2030047311@qq.com>
2023-05-20 14:33:52 +0800

    feat: update ssl crt

--- server/proxy.js
+++ server/proxy.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
  */
-
+const fs = require("fs");
 const http = require('http')
 
 const { getServerConfig } = require('./libs/utils')
